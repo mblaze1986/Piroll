b947966d5164a433cf0696c74173d636f358f21a
Dmitrii Ishchenko <65356843+mblaze1986@users.noreply.github.com>
2020-06-08 18:47:44 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+  
+  
+    /* FIXED HEADER */
+    $(window).on("scroll load resize", function () {
+      introH = intro.innerHeight();
+      scrollPos = $(this).scrollTop();
+  
+      if (scrollPos > introH) {
+        header.addClass("fixed");
+      } else {
+        header.removeClass("fixed");
+      }
+    });
+  
+  
+  
+    /* SMOOTH SCROLL */
+    $("[data-scroll]").on("click", function (event) {
+      event.preventDefault();
+  
+      let elementId = $(this).data('scroll');
+      let elementOffset = $(elementId).offset().top;
+
+      $("#menu a.active").removeClass("active");
+      $(this).addClass("active");
+  
+      $("html, body").animate({
+        scrollTop: elementOffset - 80
+      }, 700);
+    });
+
+
+  /* Menu nav toggle */
+    $("#nav_toggle").on("click", function (event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#menu").toggleClass("active");
+        $("#header").toggleClass("active");
+    });
+
+
+    $("[data-slider]").slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        // autoplay: true,
+        // autoplaySpeed: 2000
+    });
+
+
+});
